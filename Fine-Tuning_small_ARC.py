2cd1900082bdc2afa58142475132a11a63d10d46
Your Name <you@example.com>
2024-01-28 03:58:37 +0000

    2024-01-28

---
+++ Fine-Tuning_small_ARC.py
@@ -0,0 +1,146 @@
+import time
+import random
+import pandas as pd
+from glob import glob
+import argparse
+import json
+import subprocess
+import sys
+import os
+import tensorflow as tf
+from transformers import DistilBertTokenizer
+from transformers import TFDistilBertForSequenceClassification
+from transformers import DistilBertConfig
+import sys
+
+args = sys.argv
+
+# myDir = "/mnt/c/Users/xxxxx/Downloads/"
+myDir = args[1]
+myParquet = myDir + "/amazon_reviews_2015.snappy.parquet"
+
+# os.environ['CUDA_VISIBLE_DEVICE'] = '-1'
+
+epochs = 32
+steps_per_epoch = 128
+validation_steps = 128
+test_steps = 128
+freeze_bert_layer = True
+learning_rate = 3e-3
+epsilon = 1e-08
+max_seq_length = 64
+
+def select_data_and_label_from_record(record):
+    x = {
+        "input_ids": record["input_ids"],
+        "input_mask": record["input_mask"],
+    }
+    y = record["label_ids"]
+    return (x, y)
+
+def file_based_input_dataset_builder(channel, input_filenames, max_seq_length, is_training, drop_remainder):
+    dataset = tf.data.TFRecordDataset(input_filenames)
+    dataset = dataset.repeat(100)
+    dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+
+    name_to_features = {
+        "input_ids": tf.io.FixedLenFeature([max_seq_length], tf.int64),
+        "input_mask": tf.io.FixedLenFeature([max_seq_length], tf.int64),
+        "label_ids": tf.io.FixedLenFeature([], tf.int64),
+    }
+
+    def _decode_record(record, name_to_features):
+        return tf.io.parse_single_example(record, name_to_features)
+
+    dataset = dataset.apply(
+        tf.data.experimental.map_and_batch(
+            lambda record: _decode_record(record, name_to_features),
+            batch_size=8,
+            drop_remainder=drop_remainder,
+            num_parallel_calls=tf.data.experimental.AUTOTUNE,
+        )
+    )
+
+    dataset.cache()
+
+    if is_training:
+        dataset = dataset.shuffle(seed=42, buffer_size=10, reshuffle_each_iteration=True)
+
+    return dataset
+
+train_data_filenames = myDir + "/train_data_small.tfrecord"
+print("train_data_filenames {}".format(train_data_filenames))
+
+train_dataset = file_based_input_dataset_builder(
+    channel="train", 
+    input_filenames=train_data_filenames, 
+    max_seq_length=64, 
+    is_training=True, 
+    drop_remainder=False
+).map(select_data_and_label_from_record)
+
+validation_data_filenames = myDir + "/validation_data_small.tfrecord"
+print("validation_data_filenames {}".format(validation_data_filenames))
+
+validation_dataset = file_based_input_dataset_builder(
+    channel="validation",
+    input_filenames=validation_data_filenames,
+    max_seq_length=64,
+    is_training=False,
+    drop_remainder=False,
+).map(select_data_and_label_from_record)
+
+CLASSES = [1, 2, 3, 4, 5]
+
+config = DistilBertConfig.from_pretrained(
+    "distilbert-base-uncased",
+    num_labels=len(CLASSES),
+    id2label={0: 1, 1: 2, 2: 3, 3: 4, 4: 5},
+    label2id={1: 0, 2: 1, 3: 2, 4: 3, 5: 4},
+)
+
+transformer_model = TFDistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased", config=config)
+
+input_ids = tf.keras.layers.Input(shape=(max_seq_length,), name="input_ids", dtype="int32")
+input_mask = tf.keras.layers.Input(shape=(max_seq_length,), name="input_mask", dtype="int32")
+
+embedding_layer = transformer_model.distilbert(input_ids, attention_mask=input_mask)[0]
+X = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(50, return_sequences=True, dropout=0.1, recurrent_dropout=0.1))(
+    embedding_layer
+)
+X = tf.keras.layers.GlobalMaxPool1D()(X)
+X = tf.keras.layers.Dense(50, activation="relu")(X)
+X = tf.keras.layers.Dropout(0.2)(X)
+X = tf.keras.layers.Dense(len(CLASSES), activation="softmax")(X)
+
+model = tf.keras.Model(inputs=[input_ids, input_mask], outputs=X)
+
+for layer in model.layers[:3]:
+    layer.trainable = not freeze_bert_layer
+
+loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+metric = tf.keras.metrics.SparseCategoricalAccuracy("accuracy")
+optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate, epsilon=epsilon)
+model.compile(optimizer=optimizer, loss=loss, metrics=[metric])
+model.summary()
+
+callbacks = []
+
+log_dir = "./tmp/tensorboard/"
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
+callbacks.append(tensorboard_callback)
+
+#with tf.device('/CPU:0'):
+#with tf.device('/XPU:0'):
+history = model.fit(
+    train_dataset,
+    shuffle=True,
+    epochs=epochs,
+    steps_per_epoch=steps_per_epoch,
+    validation_data=validation_dataset,
+    validation_steps=validation_steps,
+    callbacks=callbacks,
+)
+
+model.save(myDir + '/tensorflow_small/',save_format='tf')
+
